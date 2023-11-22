1f3857e4237376aac5a76f58ee3b46e8d88437cc
1sahinomer1 <1sahinomer1@gmail.com>
2023-11-23 00:33:29 +0300

    feat : add redirect onclick to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { Flex, Typography } from "antd";
+import { useNavigate } from "react-router-dom";
 
 import { useAppSelector } from "store/hooks";
 
@@ -9,11 +10,15 @@ import { baseStyle, titleStyle } from "./styles";
 const Header = () => {
   const { Title } = Typography;
 
+  const navigate = useNavigate();
   const { fullName } = useAppSelector(state => state.user);
 
   return (
     <Flex style={baseStyle} justify="space-between">
-      <Logo />
+      <Logo
+        onClick={() => navigate("/packages")}
+        style={{ cursor: "pointer" }}
+      />
       <Flex>
         <User />
         <Title level={5} style={titleStyle}>
